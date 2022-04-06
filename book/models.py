e36f1530593cb579654ebfe3ec1944d9337c8076
hjx <2449439561@qq.com>
2022-04-06 13:29:37 +0800

    新建Book和Person类并迁移

--- book/models.py
+++ book/models.py
@@ -1,3 +1,13 @@
+import django.db.models
 from django.db import models
 
+
 # Create your models here.
+class Book(models.Model):
+    name = models.CharField(max_length=10, name="书名")
+
+
+class Person(models.Model):
+    name = models.CharField(max_length=10, name="名字")
+    gender = models.BooleanField(name="性别")
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, name="关联的书目")
